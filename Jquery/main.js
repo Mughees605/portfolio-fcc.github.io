352df3840018f93cffd5e1614e6c372770ff7080
Hafiz Mughees Siddiqui <mughees605@gmail.com>
2016-09-16 03:37:38 +0300

    second commit

--- Jquery/main.js
+++ Jquery/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.nav-pills li').addClass('pull-right');
+    $('body .nav-pills').on('click','.sla',function(){
+    $('.pageOne').slideUp('slow');
+    $('.pageTwo').slideDown('slow');
+    })
+     $('body .nav-pills').on('click','.slh',function(){
+    $('.pageOne').slideDown('slow');
+    })
 })
