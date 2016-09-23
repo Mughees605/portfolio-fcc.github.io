8f45c55e2da4a070062ac19250032970a4f9f646
Hafiz Mughees Siddiqui <mughees605@gmail.com>
2016-09-23 03:07:27 +0300

    commit

--- Jquery/main.js
+++ Jquery/main.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
     $('.nav-pills li').addClass('pull-right');
-    $('body .nav-pills').on('click','.sla',function(){
-    $('.pageOne').slideUp('slow');
-    $('.pageTwo').slideDown('slow');
-    })
-     $('body .nav-pills').on('click','.slh',function(){
-    $('.pageOne').slideDown('slow');
-    })
-})
+    
+});
